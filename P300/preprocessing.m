600e8219d864faebea83231b1641c54d15715ad2
zivtam <94539228+zivtam@users.noreply.github.com>
2022-03-30 19:32:07 +0300

    Add files via upload

---
+++ P300/preprocessing.m
@@ -0,0 +1,25 @@
+function [EEG] = preprocessing(EEG, Hz, highLim, lowLim, down_srate)
+    srate = Hz; 
+    
+    % low-pass filter
+    EEG = pop_eegfiltnew(EEG, 'hicutoff',highLim,'plotfreqz',1);    % removes data above
+    EEG = eeg_checkset(EEG);
+
+    % High-pass filter
+    EEG = pop_eegfiltnew(EEG, 'locutoff',lowLim,'plotfreqz',1);     % removes data under
+    EEG = eeg_checkset(EEG);
+    
+%     %Zero-phase digital filtering
+%     EEG = filtfilt(EEG,b,a); %ask Ophir
+%     EEG = eeg_checkset(EEG);
+    
+    %downsample data
+    if srate > down_srate
+        EEG = pop_resample(EEG, down_srate);
+    end
+    EEG = eeg_checkset(EEG);
+    
+    %Median Filtering
+    %Facet Method
+    
+   
