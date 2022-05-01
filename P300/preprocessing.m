3fa6a57b9bfddada87d3b32187db5d259b188f34
Ornahm <59653355+Ornahm@users.noreply.github.com>
2022-05-01 12:12:00 +0300

    average trigs

--- P300/preprocessing.m
+++ P300/preprocessing.m
@@ -1,4 +1,4 @@
-function [EEG] = Preprocessing(EEG, triggersTime, trainingVector)
+function [EEG, meanTrigs] = Preprocessing(EEG, triggersTime, trainingVector)
 % Preprocessing - all the preprocessing done on recorded data
 %
 %INPUT:
@@ -14,9 +14,18 @@ function [EEG] = Preprocessing(EEG, triggersTime, trainingVector)
     % This code needs to be fixed to new EEG shape
     EEG = pop_eegfiltnew(EEG, 'hicutoff', Utils.Config.highLim,'plotfreqz',1); % low pass
     EEG = pop_eegfiltnew(EEG, 'locutoff',Utils.Config.lowLim,'plotfreqz',1);  % high pass
-%     
+
     % Spliting the trials must be the first thing to happen to allow for correct splitting because splitting is based on time stamps
     EEG = splitTrials(EEG, triggersTime);
+
+    % Average trigger signals per class
+    classes = unique(trainingVector);
+    meanTrigs = zeros(numTrials, length(classes), eegChannels, windowSize);
+    for currTrial=1:numTrials    
+        for class = classes
+            meanTrigs(currTrial,class,:,:) = mean(EEG(currTrial,trainingVector(currTrial) == class,:,:),2);
+        end
+    end
     
     % downsample
 %     if Hz > Utils.Config.down_srate
